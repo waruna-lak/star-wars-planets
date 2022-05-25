bb449e074de4ab7e2ac5eba5393a172d808a8e45
Waruna Jayasinghe <waruna.emedia@gmail.com>
2022-05-25 17:14:04 +0530

    create application class

---
+++ app/src/main/java/com/waruna/starwarsplanets/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.waruna.starwarsplanets
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Waruna Jayasinghe on 5/25/2022.
+ */
+@HiltAndroidApp
+class MyApplication : Application() {
+}
