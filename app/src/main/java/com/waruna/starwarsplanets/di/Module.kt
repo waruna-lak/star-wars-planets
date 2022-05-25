c4f4b393a22cca7815715766c96d959ee553ca58
Waruna Jayasinghe <waruna.emedia@gmail.com>
2022-05-25 17:12:37 +0530

    create hilt module

---
+++ app/src/main/java/com/waruna/starwarsplanets/di/Module.kt
@@ -0,0 +1,56 @@
+package com.waruna.starwarsplanets.di
+
+import com.waruna.starwarsplanets.network.PlanetApi
+import com.waruna.starwarsplanets.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+/**
+ * Created by Waruna Jayasinghe on 5/25/2022.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object Module {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ) : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): PlanetApi {
+        return retrofit.create(PlanetApi::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/waruna/starwarsplanets/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.waruna.starwarsplanets.util
+
+/**
+ * Created by Waruna Jayasinghe on 5/25/2022.
+ */
+object Constants {
+    const val BASE_URL = "https://swapi.dev/api/"
+}
